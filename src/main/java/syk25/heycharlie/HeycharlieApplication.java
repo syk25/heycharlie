1d8d14f15251bfb04bf22fa1658ce410a345c4c4
syk25 <syk2245@gmail.com>
2024-01-31 17:30:16 +0900

    init: create project

---
+++ src/main/java/syk25/heycharlie/HeycharlieApplication.java
@@ -0,0 +1,13 @@
+package syk25.heycharlie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeycharlieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeycharlieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/syk25/heycharlie/HeycharlieApplicationTests.java
@@ -0,0 +1,13 @@
+package syk25.heycharlie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeycharlieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
